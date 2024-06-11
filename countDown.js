7d4ef0d57a58a3217ff6a7e99361f047d87e6634
unknown <krithikroshan113@gmail.com>
2024-06-11 21:16:04 +0530

    countdown

--- countDown.js
+++ countDown.js
@@ -8,7 +8,9 @@ let interval = setInterval(() => {
     if (count == 0) {
         heading.innerText = '"Your mobile has hacked successfully"'
         document.body.style.background = "hsla(0, 57%, 43%, 0.713)";
-        heading.style.color= 'whitesmoke';
+        heading.style.color = "whitesmoke";
+        heading.style.textDecoration = 'underline'; 
+        
         clearInterval(interval)
     }
 },1000)
